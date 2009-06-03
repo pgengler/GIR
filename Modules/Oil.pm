2d18587c02a1886403d710de72c2d6d55001b02e
pgengler <pgengler@537a7894-60ab-4413-97f5-43bf7c795e4e>
2009-06-03 15:27:55 +0000

    Add way to check current price of oil (Todo #1398)

---
+++ Modules/Oil.pm
@@ -0,0 +1,38 @@
+package Modules::Oil;
+
+use strict;
+
+use LWP::Simple;
+
+sub new()
+{
+	my $pkg = shift;
+	my $obj = { };
+	bless $obj, $pkg;
+	return $obj;
+}
+
+sub register()
+{
+	my $this = shift;
+
+	&Modules::register_action('how much is oil', \&Modules::Oil::fetch);
+}
+
+sub fetch()
+{
+	my ($type, $user, $data, $where) = @_;
+
+	my $url = 'http://www.howmuchisoil.com/csv.cgi';
+
+	my $result = get($url);
+
+	if ($result) {
+		chomp $result;
+		my ($price, $change, $pchange, $date, $time) = split(/,/, $result);
+
+		return sprintf('As of %s on %s, oil was at $%s, %s $%s (%s%%)', $time, $date, $price, ($change > 0) ? 'up' : 'down', abs($change), abs($pchange));
+	}
+}
+
+1;
