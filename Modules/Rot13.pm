561e7ac786c7fabdb774ad065bf2383eeb343c65
pgengler <pgengler@537a7894-60ab-4413-97f5-43bf7c795e4e>
2009-07-10 16:59:37 +0000

    Add ROT13 module (Todo #1516)

---
+++ Modules/Rot13.pm
@@ -0,0 +1,42 @@
+package Modules::ROT13;
+
+#######
+## PERL SETUP
+#######
+use strict;
+
+##############
+sub new()
+{
+	my $pkg = shift;
+	my $obj = { };
+	bless $obj, $pkg;
+	return $obj;
+}
+
+sub register()
+{
+	my $this = shift;
+
+	&Modules::register_action('rot13', \&Modules::ROT13::rot13);
+
+	&Modules::register_help('rot13', \&Modules::ROT13::help);
+}
+
+sub rot13()
+{
+	my ($type, $user, $data, $where) = @_;
+
+	$data = 
+
+	return $data;
+}
+
+sub help()
+{
+	my ($type, $user, $data, $where, $addressed) = @_;
+
+	return "'rot13 <text>': Performs the ROT13 operation on the given text.";
+}
+
+1;
