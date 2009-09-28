f71326b832ffc9fe4fe5445e1a244fd486721036
pgengler <pgengler@537a7894-60ab-4413-97f5-43bf7c795e4e>
2009-09-28 03:00:12 +0000

    Initial checkin; includes fix for Todo #1834 (Don't display help text unless privately or explicitly addressed)

---
+++ Modules/Help.pm
@@ -0,0 +1,38 @@
+package Modules::Help;
+
+use strict;
+
+sub new()
+{
+	my $pkg = shift;
+	my $obj = { };
+	bless $obj, $pkg;
+	return $obj;
+}
+
+sub register()
+{
+	my $this = shift;
+
+	&Modules::register_action('help', \&Modules::Help::process);
+}
+
+sub process()
+{
+	my ($type, $user, $data, $where, $addressed) = @_;
+
+	return unless ($addressed || $type eq 'private');
+
+	if ($data) {
+		if ($Modules::help_functions{ $data }) {
+			return $Modules::help_functions{ $data }->($type, $user, $data, $where, $addressed);
+		} elsif ($addressed || $type eq 'private') {
+			return "No help is available for '$data'";
+		}
+	} else {
+		my @topics = sort { $a cmp $b } keys %Modules::help_functions;
+		return 'Type "help <command>" for help on a specific command; available commands are: ' . join(', ', @topics);
+	}
+}
+
+1;
