09adc09935d8cb521ffbf2670907542a259e5b9e
pgengler <pgengler@537a7894-60ab-4413-97f5-43bf7c795e4e>
2010-02-18 21:32:44 +0000

    Don't require a comma or colon before a trailing nickname to consider a message addressed; whitespace is acceptable too (Todo #2111)

--- Main/Bot.pl
+++ Main/Bot.pl
@@ -289,7 +289,7 @@ sub message()
 	if ($message =~ /^\s*$config->{'nick'}(\,|\:|\s)\s*(.+)$/i) {
 		$addressed = 1;
 		$message = $2;
-	} elsif ($message =~ /(.+)(\,|\:)\s*$config->{'nick'}(\.|\?|\!)?\s*$/i) {
+	} elsif ($message =~ /(.+?)(\,|\:|\s+)\s*$config->{'nick'}(\.|\?|\!)?\s*$/i) {
 		$addressed = 1;
 		$message = $1;
 	}
