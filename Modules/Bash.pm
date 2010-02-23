c64ef43918bdbde1f27af913297962314b4c15aa
pgengler <pgengler@537a7894-60ab-4413-97f5-43bf7c795e4e>
2010-02-23 02:54:57 +0000

    Convert <br /> elements to newlines (Todo #2134)

--- Modules/Bash.pm
+++ Modules/Bash.pm
@@ -14,16 +14,6 @@ use Fcntl;
 use HTML::Entities;
 use LWP::UserAgent;
 
-#######
-## GLOBALS
-#######
-my $no_agent = 0;
-
-BEGIN {
-	eval "use LWP::UserAgent";
-	$no_agent++ if ($@);
-}
-
 ##############
 sub new()
 {
@@ -53,50 +43,47 @@ sub process()
 
 	my $result;
 
-	if ($no_agent) {
-		return 0;
-	} else {
-		my %quotes;
-		tie(%quotes, 'AnyDBM_File', $Bot::config->{'data_dir'} . '/bashquotes', O_RDWR|O_CREAT);
+	my %quotes;
+	tie(%quotes, 'AnyDBM_File', $Bot::config->{'data_dir'} . '/bashquotes', O_RDWR|O_CREAT);
 
-		# Check if we have this one cached
-		if ($quotes{ $data }) {
-			my $result = $quotes{ $data };
-			untie %quotes;
-			return $result;
-		}
-
-		# Fetch from bash.org
-		my $ua = new LWP::UserAgent;
-#		if (my $proxy = Bot::getparam('httpproxy')) {
-#			$ua->proxy('http', $proxy)
-#		};
-
-		$ua->timeout(10);
-		my $request = new HTTP::Request('GET', "http://bash.org/?$data");
-		my $response = $ua->request($request); 
-
-		if (!$response->is_success) {
-			untie %quotes;
-			return "Something failed in connecting to bash.org. Try again later.";
-		}
-
-		my $content = $response->content;
-
-		if ($content =~ /Quote #$data was rejected/ || $content =~ /Quote #$data does not exist/ || $content =~ /Quote #$data is pending moderation/) {
-			untie %quotes;
-			return "Couldn't get quote $data. It probably doesn't exist";
-		}
-
-		if ($content =~ /\<p class=\"qt\"\>(.+?)\<\/p\>/s) {
-			$result = &HTML::Entities::decode_entities($1);
-			$quotes{ $data } = $result;
-		} else {
-			$result = "Couldn't get quote $data. It probably doesn't exist.";
-		}
+	# Check if we have this one cached
+	if ($quotes{ $data }) {
+		my $result = $quotes{ $data };
 		untie %quotes;
 		return $result;
 	}
+
+	# Fetch from bash.org
+	my $ua = new LWP::UserAgent;
+#	if (my $proxy = Bot::getparam('httpproxy')) {
+#		$ua->proxy('http', $proxy)
+#	};
+
+	$ua->timeout(10);
+	my $request = new HTTP::Request('GET', "http://bash.org/?$data");
+	my $response = $ua->request($request); 
+
+	if (!$response->is_success) {
+		untie %quotes;
+		return "Something failed in connecting to bash.org. Try again later.";
+	}
+
+	my $content = $response->content;
+
+	if ($content =~ /Quote #$data was rejected/ || $content =~ /Quote #$data does not exist/ || $content =~ /Quote #$data is pending moderation/) {
+		untie %quotes;
+		return "Couldn't get quote $data. It probably doesn't exist";
+	}
+
+	if ($content =~ /\<p class=\"qt\"\>(.+?)\<\/p\>/s) {
+		$result = &HTML::Entities::decode_entities($1);
+		$result =~ s/\<br \/\>/\n/g;
+		$quotes{ $data } = $result;
+	} else {
+		$result = "Couldn't get quote $data. It probably doesn't exist.";
+	}
+	untie %quotes;
+	return $result;
 }
 
 sub help()
