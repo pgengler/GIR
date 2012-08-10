48df12f959847df17775090daafcba99b5b510ef
Phil Gengler <phil@pgengler.net>
2012-08-10 12:00:52 -0400

    Move command-parsing functionality from Console::console_parse to Command::parse

---
+++ lib/Command.pm
@@ -0,0 +1,52 @@
+package Command;
+
+use strict;
+
+sub parse($)
+{
+	my ($string) = @_;
+
+	chomp $string;
+
+	# For now, nothing fancy, just some simple string matches
+	if ($string =~ /^quit(\s+(.+))?$/) {
+		return _command_string('quit', $2);
+	} elsif ($string =~ /^reload(\s+(.+))?\s*$/) {
+		return _command_string('reload', $2);
+	} elsif ($string =~ /^unload\s+(.+)\s*$/) {
+		return _command_string('unload', $1);
+	} elsif ($string =~ /^load\s+(.+)\s*$/) {
+		return _command_string('load', $1);
+	} elsif ($string =~ /^\s*part\s+(.+?)(\s+(.+))?$/i) {
+		return _command_string('part', $1, $3);
+	} elsif ($string =~ /^\s*join\s+(.+)$/i) {
+		return _command_string('join', $1);
+	} elsif ($string =~ /^\s*say\s+(.+?)\s+(.+)$/i) {
+		return _command_string('say', $1, $2);
+	} elsif ($string =~ /^\s*action\s+(.+)\s+(.+)$/i) {
+		return _command_string('action', $1, $2);
+	} elsif ($string =~ /^\s*discon(nect)?(\s+(.+))?$/i) {
+		return _command_string('discon', $3);
+	} elsif ($string =~ /^\s*connect\s*$/i) {
+		return _command_string('connect');
+	} elsif ($string =~ /^\s*nick\s+(.+)$/) {
+		return _command_string('nick', $1);
+	} elsif ($string =~ /^\s*debug\s+(on|off)\s*$/) {
+return _command_string('debug', $1);
+	}
+
+	Bot::status("Unrecognized command '%s'", $string);
+}
+
+sub _command_string($@)
+{
+	my ($command, @parameters) = @_;
+
+	if (scalar(@parameters) == 0) {
+		return $command;
+	}
+
+	return join('||', map { $_ || '' } ($command, @parameters));
+}
+
+1;
--- lib/Console.pm
+++ lib/Console.pm
@@ -2,6 +2,8 @@ package Console;
 
 use strict;
 
+use Command;
+
 ##############
 ## CONSOLE INPUT
 ##############
@@ -26,41 +28,13 @@ sub console_parse($)
 {
 	my $str = shift;
 
-	chomp $str;
+	my $command = Command::parse($str);
 
-	# For now, nothing fancy, just some simple string matches
-	if ($str =~ /^quit(\s+(.+))?$/) {
-		Bot::status("Shutting down");
-		if ($2) {
-			push @Bot::commands, "quit||$2";
-		}
+	push @Bot::commands, $command;
+
+	if ($command =~ /^quit/) {
 		$Bot::bot->kill('SIGTERM');
 		threads->exit();
-	} elsif ($str =~ /^reload(\s+(.+))?\s*$/) {
-		push @Bot::commands, "reload||" . ($2 || '');
-	} elsif ($str =~ /^unload\s+(.+)\s*$/) {
-		push @Bot::commands, "unload||$1";
-	} elsif ($str =~ /^load\s+(.+)\s*$/) {
-		push @Bot::commands, "load||$1";
-	} elsif ($str =~ /^\s*part\s+(.+?)(\s+(.+))?$/i) {
-		push @Bot::commands, "part||$1||" . ($3 || '');
-	} elsif ($str =~ /^\s*join\s+(.+)$/i) {
-		push @Bot::commands, "join||$1";
-	} elsif ($str =~ /^\s*say\s+(.+?)\s+(.+)$/i) {
-		push @Bot::commands, "say||$1||$2";
-	} elsif ($str =~ /^\s*action\s+(.+)\s+(.+)$/i) {
-		push @Bot::commands, "action||$1||$2";
-	} elsif ($str =~ /^\s*discon(nect)?(\s+(.+))?$/i) {
-		my $reason = $3 || $1 || '';
-		push @Bot::commands, "discon||$reason";
-	} elsif ($str =~ /^\s*connect\s*$/i) {
-		push @Bot::commands, "connect||";
-	} elsif ($str =~ /^\s*nick\s+(.+)$/) {
-		push @Bot::commands, "nick||$1";
-	} elsif ($str =~ /^\s*debug\s+(on|off)\s*$/) {
-		push @Bot::commands, "debug||$1";
-	} else {
-		Bot::status("Unrecognized command");
 	}
 
 	if (scalar(@Bot::commands) > 0) {
