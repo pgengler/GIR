e8a791d92d98aa98810581abae6eb40f8d7726e9
Phil Gengler <phil@pgengler.net>
2012-08-10 12:44:16 -0400

    Move data for Eightball module into a __DATA__ section rather than a separate file

--- Modules/Eightball.pm
+++ Modules/Eightball.pm
@@ -1,8 +1,7 @@
 package Modules::Eightball;
 
 use strict;
-
-my @answers;
+use feature 'state';
 
 sub new()
 {
@@ -26,21 +25,10 @@ sub process($)
 {
 	my $message = shift;
 
-	# Check if answers are loaded
-	unless (@answers) {
-		my $answer_file = $Bot::config->{'data_dir'} . '/8ball.txt';
-		open(my $fh, '<', $answer_file) or do { Bot::error("Modules::Eightball can't read 8ball.txt: %s", $!); return undef };
-		while (<$fh>) {
-			chomp;
-			push @answers, $_;
-		}
-		close($fh);
-	}
+	my @responses = load_responses();
 
-	if (@answers) {
-		# Get a random response
-		return $answers[rand(@answers)];
-	}
+	# Get a random response
+	return $responses[rand(@responses)];
 }
 
 sub help($)
@@ -50,4 +38,66 @@ sub help($)
 	return "'8ball <question>': Used a magic 8-ball to try to divine an answer to your question.";
 }
 
+sub load_responses()
+{
+	state $data_position = tell DATA;
+
+	my @storage;
+
+	while (my $line = <DATA>) {
+		chomp $line;
+		push @storage, $line;
+	}
+
+	seek DATA, $data_position, 0;
+
+	return @storage;
+}
+
 1;
+
+__DATA__
+Outlook Not So Good
+My Reply Is No
+Don't Count On It
+You May Rely On It
+Ask Again Later
+Most Likely
+Cannot Predict Now
+Yes
+Yes Definitely
+Better Not Tell You Now
+It Is Certain
+Very Doubtful
+It Is Decidedly So
+Concentrate and Ask Again
+Signs Point to Yes
+My Sources Say No
+Without a Doubt
+Reply Hazy, Try Again
+As I See It, Yes
+NOT
+What do YOU think
+Obviously
+Ask me if I care
+Yeah, and I'm the Pope
+That's ridiculous
+Who cares
+Forget about it
+You wish
+Yeah, right
+Sure
+Get a clue
+In your dreams
+Oh, please
+Whatever
+As if
+You've got to be kidding
+Dumb question.  Ask another
+Not a chance
+Outlook Sucks
+THIS SPACE FOR RENT
+Bugger Off
+How appropriate, you fight like a cow
+Eat more cheese, then ask again
+When hell freezes over
