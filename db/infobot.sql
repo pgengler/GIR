aabc4ed1423e2f8ad433f1f4476d2bff0834bd3c
Phil Gengler <phil@pgengler.net>
2013-03-19 20:48:26 -0400

    Update SQL for 'infobot' table for Postgres

--- db/infobot.sql
+++ db/infobot.sql
@@ -1,6 +1,7 @@
+CREATE TYPE relation AS enum ('is', 'are');
 CREATE TABLE infobot (
 	phrase VARCHAR(255) NOT NULL,
-	relates ENUM('is', 'are') NOT NULL DEFAULT 'is',
+	relates relation NOT NULL DEFAULT 'is',
 	value TEXT NULL,
-	locked TINYINT(1) NOT NULL DEFAULT 0
+	locked BOOLEAN DEFAULT false
 );
