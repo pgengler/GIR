018b74e2a514796748b82df9aecb0ce0c8548352
Phil Gengler <phil@pgengler.net>
2017-04-18 10:39:14 -0400

    [Git module] Add 'push' as a recognized git command

--- Modules/Git.pm
+++ Modules/Git.pm
@@ -17,7 +17,7 @@ sub fake_git_output
 			return "ERROR: Repository not found.\nfatal: Could not read from remote repository.\n\nPlease make sure you have the correct access rights\nand the repository exists.";
 		} elsif ($command eq 'init') {
 			return '/home/gir/.git: Permission denied';
-		} elsif ($command eq 'pull' || $command eq 'fetch' || $command eq 'checkout' || $command eq 'branch' || $command eq 'add' || $command eq 'mv' || $command eq 'reset' || $command eq 'rm' || $command eq 'diff' || $command eq 'rebase' || $command eq 'merge' || $command eq 'bisect' || $command eq 'commit') {
+		} elsif ($command eq 'pull' || $command eq 'push' || $command eq 'fetch' || $command eq 'checkout' || $command eq 'branch' || $command eq 'add' || $command eq 'mv' || $command eq 'reset' || $command eq 'rm' || $command eq 'diff' || $command eq 'rebase' || $command eq 'merge' || $command eq 'bisect' || $command eq 'commit') {
 			return 'fatal: Not a git repository (or any of the parent directories): .git';
 		} elsif ($command eq '--help') {
 			return "Here's a tip: use a Git client instead of an IRC bot.";
